b0e4bf2ac7cd754954fc0ef3129e944053e7ef7d
gisele <gisesonia@gmail.com>
2023-10-23 13:00:15 -0300

    corrigindo url do back4app

--- lib/repositories/contact_repository.dart
+++ lib/repositories/contact_repository.dart
@@ -3,11 +3,11 @@ import '../model/contact.dart';
 
 class ContactRepository {
   final _customDio = Back4AppCustomDio();
+  final String url = '/contact';
 
   ContactRepository();
 
   Future<List<Contact>> getContacts() async {
-    final url = '/contact';
     final result = await _customDio.dio.get(url);
     final data = result.data['results'] as List;
     final contacts = data.map((item) => Contact.fromJson(item)).toList();
@@ -17,7 +17,7 @@ class ContactRepository {
   Future<void> createContact(Contact contact) async {
     try {
       await _customDio.dio.post(
-        '/contact',
+        url,
         data: contact.toJson(),
       );
     } catch (e) {
@@ -28,7 +28,7 @@ class ContactRepository {
   Future<void> updateContact(Contact contact) async {
     try {
       await _customDio.dio.put(
-        '/classes/Pessoa/${contact.results?.first.objectId}', // Substitua 'Pessoa' pelo nome da sua classe no Back4App.
+        "$url${contact.results?.first.objectId}",
         data: contact.toJson(),
       );
     } catch (e) {
@@ -39,7 +39,7 @@ class ContactRepository {
   Future<void> deleteContact(String objectId) async {
     try {
       await _customDio.dio.delete(
-        '/contact/$objectId', // Substitua 'Pessoa' pelo nome da sua classe no Back4App.
+        "$url$objectId",
       );
     } catch (e) {
       throw e;
