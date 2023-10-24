5d70408a9301f367e9f589b35e24326e69c7daeb
gisele <gisesonia@gmail.com>
2023-10-24 17:43:25 -0300

    Alteração na tela de editar

--- lib/pages/contact_list.dart
+++ lib/pages/contact_list.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:contatosapp/repositories/contact_repository.dart';
 import 'package:contatosapp/widgets/contact_item.dart';
 import 'package:flutter/material.dart';
@@ -58,11 +59,132 @@ class _ContactListState extends State<ContactList> {
                                 key: Key(contact.objectId),
                                 child: ContactItem(
                                   contact: contact,
+                                  onEditPressed: () async {
+                                    final ancestor =
+                                        context.findRootAncestorStateOfType<
+                                            _ContactListState>();
+                                    await showEditDialog(context, contact);
+                                    ancestor?.getContacts();
+                                  },
                                 ));
                           }),
                     ),
             ],
           ),
         ));
+  }
+
+  Future<void> showEditDialog(BuildContext context, contact) async {
+    TextEditingController nameController =
+        TextEditingController(text: contact.name);
+    TextEditingController phoneController =
+        TextEditingController(text: contact.phone);
+    TextEditingController emailController =
+        TextEditingController(text: contact.email);
+    TextEditingController urlAvatarController =
+        TextEditingController(text: contact.urlavatar);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: const Text("Editar Contato"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      _showImagePreview(urlAvatarController.text, context);
+                    },
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: urlAvatarController.text.isNotEmpty
+                          ? CachedNetworkImageProvider(urlAvatarController.text)
+                          : null,
+                      child: urlAvatarController.text.isNotEmpty
+                          ? null
+                          : const Icon(
+                              Icons.camera,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(labelText: 'Name'),
+                  ),
+                  TextFormField(
+                    controller: phoneController,
+                    decoration: const InputDecoration(labelText: 'Phone'),
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    controller: urlAvatarController,
+                    decoration: const InputDecoration(
+                        labelText: 'URL da Imagem de Perfil'),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: const Text("Save"),
+                  onPressed: () async {
+                    // Update the contact with the edited data
+                    contact.name = nameController.text;
+                    contact.phone = phoneController.text;
+                    contact.email = emailController.text;
+                    contact.urlavatar = urlAvatarController.text;
+
+                    // Limpe a imagem da URL
+                    await ContactRepository().updateContact(contact);
+
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _showImagePreview(String url, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Pré-Visualização da Imagem'),
+          content: url.isNotEmpty
+              ? CachedNetworkImage(
+                  imageUrl: url,
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                )
+              : const Text('Nenhuma URL fornecida'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/widgets/contact_item.dart
+++ lib/widgets/contact_item.dart
@@ -1,12 +1,12 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:contatosapp/model/contact.dart';
-import 'package:contatosapp/repositories/contact_repository.dart';
 import 'package:flutter/material.dart';
 
 class ContactItem extends StatelessWidget {
   final Contact contact;
+  final VoidCallback onEditPressed;
 
-  const ContactItem({Key? key, required this.contact}) : super(key: key);
+  const ContactItem(
+      {super.key, required this.contact, required this.onEditPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -29,127 +29,11 @@ class ContactItem extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.edit),
             onPressed: () {
-              showEditDialog(context, contact);
+              onEditPressed();
             },
           ),
         ],
       ),
-    );
-  }
-
-  void showEditDialog(BuildContext context, Contact contact) {
-    TextEditingController nameController =
-        TextEditingController(text: contact.name);
-    TextEditingController phoneController =
-        TextEditingController(text: contact.phone);
-    TextEditingController emailController =
-        TextEditingController(text: contact.email);
-    TextEditingController urlAvatarController =
-        TextEditingController(text: contact.urlavatar);
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: const Text("Editar Contato"),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _showImagePreview(urlAvatarController.text, context);
-                    },
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: urlAvatarController.text.isNotEmpty
-                          ? CachedNetworkImageProvider(urlAvatarController.text)
-                          : null,
-                      child: urlAvatarController.text.isNotEmpty
-                          ? null
-                          : const Icon(
-                              Icons.camera,
-                              size: 40,
-                              color: Colors.white,
-                            ),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(labelText: 'Name'),
-                  ),
-                  TextFormField(
-                    controller: phoneController,
-                    decoration: const InputDecoration(labelText: 'Phone'),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(labelText: 'Email'),
-                  ),
-                  TextFormField(
-                    controller: urlAvatarController,
-                    decoration: const InputDecoration(
-                        labelText: 'URL da Imagem de Perfil'),
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text("Cancel"),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                  child: const Text("Save"),
-                  onPressed: () {
-                    // Update the contact with the edited data
-                    contact.name = nameController.text;
-                    contact.phone = phoneController.text;
-                    contact.email = emailController.text;
-                    contact.urlavatar = urlAvatarController.text;
-
-                    setState(() {
-                      // Limpe a imagem da URL
-                      ContactRepository().updateContact(contact);
-                    });
-
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
-  }
-
-  void _showImagePreview(String url, BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Pré-Visualização da Imagem'),
-          content: url.isNotEmpty
-              ? CachedNetworkImage(
-                  imageUrl: url,
-                  placeholder: (context, url) =>
-                      const CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                )
-              : const Text('Nenhuma URL fornecida'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Fechar'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
